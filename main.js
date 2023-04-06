47a55f7eec2f16ec84cc9f1002134da37f47117a
noahschofield <71097004+noahschofield@users.noreply.github.com>
2023-04-06 17:44:34 -0400

    work for website

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.css'
+
+const enchance = id => {
+  const element = document.getElementById(id),
+        text = element.innerText.split("");
+
+  element.innerText = "";
+
+  text.forEach(letter => {
+    const span = document.createElement("span");
+
+    span.className = "letter";
+
+    span.innerText = letter;
+
+    element.appendChild(span);
+  });
+}
+
+enchance("channel-link");
